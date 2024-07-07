b5507987b0460718c1f7991f5e971e518a6cae00
Tbagrzego <kudlatyicss@gmail.com>
2024-07-07 14:29:26 +0200

    Move pipelines folder to another repo

--- Terraform_stack_pipelines/terraform_init/output.tf
+++
@@ -1,9 +0,0 @@
-output "jenkins_instance_public_ip" {
-  description = "The public IP addresses of the master instances"
-  value       = aws_instance.jenkins_instance.public_ip
-}
-
-output "jenkins_private_key_pem" {
-  value     = tls_private_key.jenkins.private_key_pem
-  sensitive = true
-}
